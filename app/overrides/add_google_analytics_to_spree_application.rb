8abb39df18df8b2211218ae46db55393d0f4daa6
Duc T. Lam <ducl_13@icloud.com>
2019-02-27 15:18:42 -0800

    Separate GA into two sections because of additional GA calls in middle

--- app/overrides/add_google_analytics_to_spree_application.rb
+++ app/overrides/add_google_analytics_to_spree_application.rb
@@ -3,7 +3,12 @@ if Gem.loaded_specs['spree_core'].version >= Gem::Version.create('3.5.0.alpha')
     virtual_path: 'spree/layouts/spree_application',
     name: 'add_google_analytics_to_spree_application',
     insert_top: "[data-hook='body']",
-    partial: 'spree/shared/google_analytics',
-    original: 'cfa30a2831d9a41394c03229cd28b3c7eee69585'
+    partial: 'spree/shared/google_analytics_begin'
+  )
+  Deface::Override.new(
+    virtual_path: 'spree/layouts/spree_application',
+    name: 'add_google_analytics_end_to_spree_application',
+    insert_bottom: "[data-hook='body']",
+    partial: 'spree/shared/google_analytics_end'
   )
 end
