50bc0652c5ca10f4ad2d7307e93ffa1cac58dc6d
Duc T. Lam <ducl_13@yahoo.com>
2021-10-28 13:57:04 -0700

    Change duplicate deface name

--- app/overrides/add_google_tag_to_spree_application.rb
+++ app/overrides/add_google_tag_to_spree_application.rb
@@ -7,7 +7,7 @@ if Gem.loaded_specs['spree_core'].version >= Gem::Version.create('3.5.0.alpha')
   )
   Deface::Override.new(
     virtual_path: 'spree/layouts/spree_application',
-    name: 'add_google_analytics_to_spree_application',
+    name: 'add_google_tag_body_to_spree_application',
     insert_top: "[data-hook='body']",
     partial: 'spree/shared/google_tag_body'
   )
